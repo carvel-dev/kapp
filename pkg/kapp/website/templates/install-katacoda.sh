45ced9c3eaf7741bdc23d6d09664c3f2e8289f14
dmitriy kalinin <dkalinin@pivotal.io>
2019-03-21 11:07:41 -0700

    bump playground deps

--- pkg/kapp/website/templates/install-katacoda.sh
+++ pkg/kapp/website/templates/install-katacoda.sh
@@ -13,16 +13,16 @@ install() {
 
 	# Install ytt
 	echo "Installing ytt..."
-	wget -O- https://github.com/k14s/ytt/releases/download/v0.2.0/ytt-linux-amd64 > /tmp/ytt
-	echo "6e6e904d0966fbfe4f6c9736b84396e86d6af9edb720caca2ad34ac243d47d52  /tmp/ytt" | shasum -c -
+	wget -O- https://github.com/k14s/ytt/releases/download/v0.4.0/ytt-linux-amd64 > /tmp/ytt
+	echo "5205c22c5bd5a34c2d6f63e9870d70a7272d33d01dcbace2c5fbefcca85e00aa  /tmp/ytt" | shasum -c -
 	mv /tmp/ytt /usr/local/bin/ytt
 	chmod +x /usr/local/bin/ytt
 	echo "Installed ytt"
 
 	# Install kapp
 	echo "Installing kapp..."
-	wget -O- https://github.com/k14s/kapp/releases/download/v0.1.0/kapp-linux-amd64 > /tmp/kapp
-	echo "95c84b7d2c40830fa72dc7ff0e6f6fc31e9c7c5ab7c2bfcae22866d2967d0c8f  /tmp/kapp" | shasum -c -
+	wget -O- https://github.com/k14s/kapp/releases/download/v0.2.0/kapp-linux-amd64 > /tmp/kapp
+	echo "33d91f0326592722ccff097b059e56a76fba69171510522803aa829b0a4947c0  /tmp/kapp" | shasum -c -
 	mv /tmp/kapp /usr/local/bin/kapp
 	chmod +x /usr/local/bin/kapp
 	echo "Installed kapp"
