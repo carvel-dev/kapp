e48df4787b706ad61da1e163e3d9081e2ad9f50b
Dmitriy Kalinin <cppforlife@gmail.com>
2019-05-28 15:12:47 +0100

    clean up Corev1Pod to use typed object

--- pkg/kapp/resourcesmisc/core_v1_pod.go
+++ pkg/kapp/resourcesmisc/core_v1_pod.go
@@ -24,39 +24,45 @@ func NewCorev1Pod(resource ctlres.Resource) *Corev1Pod {
 }
 
 func (s Corev1Pod) IsDoneApplying() DoneApplyState {
-	// TODO deal with failure scenarios (retry, timeout?)
-	if phase, ok := s.resource.Status()["phase"].(string); ok {
-		switch phase {
-		// Pending: The Pod has been accepted by the Kubernetes system, but one or more of the
-		// Container images has not been created. This includes time before being scheduled as
-		// well as time spent downloading images over the network, which could take a while.
-		case "Pending":
-			return DoneApplyState{Done: false, Message: s.detailedMsg("Pending", s.pendingDetailsReason())}
+	pod := corev1.Pod{}
 
-		// Running: The Pod has been bound to a node, and all of the Containers have been created.
-		// At least one Container is still running, or is in the process of starting or restarting.
-		case "Running":
-			allTrue, msg := Conditions{s.resource}.IsSelectedTrue([]string{"Initialized", "Ready", "PodScheduled"})
-			return DoneApplyState{Done: allTrue, Successful: allTrue, Message: msg}
-
-		// Succeeded: All Containers in the Pod have terminated in success, and will not be restarted.
-		case "Succeeded":
-			return DoneApplyState{Done: true, Successful: true, Message: ""}
-
-		// Failed: All Containers in the Pod have terminated, and at least one Container has
-		// terminated in failure. That is, the Container either exited with non-zero status
-		// or was terminated by the system.
-		case "Failed":
-			return DoneApplyState{Done: true, Successful: false, Message: "Phase is failed"}
-
-		// Unknown: For some reason the state of the Pod could not be obtained,
-		// typically due to an error in communicating with the host of the Pod.
-		case "Unknown":
-			return DoneApplyState{Done: true, Successful: false, Message: "Phase is unknown"}
-		}
+	err := s.resource.AsTypedObj(&pod)
+	if err != nil {
+		return DoneApplyState{Done: true, Successful: false, Message: fmt.Sprintf("Error: Failed obj conversion: %s", err)}
 	}
 
-	return DoneApplyState{Done: false, Message: "Undetermined phase"}
+	// TODO deal with failure scenarios (retry, timeout?)
+	switch pod.Status.Phase {
+	// Pending: The Pod has been accepted by the Kubernetes system, but one or more of the
+	// Container images has not been created. This includes time before being scheduled as
+	// well as time spent downloading images over the network, which could take a while.
+	case "Pending":
+		return DoneApplyState{Done: false, Message: s.detailedMsg("Pending", s.pendingDetailsReason(pod))}
+
+	// Running: The Pod has been bound to a node, and all of the Containers have been created.
+	// At least one Container is still running, or is in the process of starting or restarting.
+	case "Running":
+		allTrue, msg := Conditions{s.resource}.IsSelectedTrue([]string{"Initialized", "Ready", "PodScheduled"})
+		return DoneApplyState{Done: allTrue, Successful: allTrue, Message: msg}
+
+	// Succeeded: All Containers in the Pod have terminated in success, and will not be restarted.
+	case "Succeeded":
+		return DoneApplyState{Done: true, Successful: true, Message: ""}
+
+	// Failed: All Containers in the Pod have terminated, and at least one Container has
+	// terminated in failure. That is, the Container either exited with non-zero status
+	// or was terminated by the system.
+	case "Failed":
+		return DoneApplyState{Done: true, Successful: false, Message: "Phase is failed"}
+
+	// Unknown: For some reason the state of the Pod could not be obtained,
+	// typically due to an error in communicating with the host of the Pod.
+	case "Unknown":
+		return DoneApplyState{Done: true, Successful: false, Message: "Phase is unknown"}
+
+	default:
+		return DoneApplyState{Done: false, Message: "Undetermined phase"}
+	}
 }
 
 func (s Corev1Pod) detailedMsg(state, msg string) string {
@@ -66,14 +72,7 @@ func (s Corev1Pod) detailedMsg(state, msg string) string {
 	return state
 }
 
-func (s Corev1Pod) pendingDetailsReason() string {
-	pod := corev1.Pod{}
-
-	err := s.resource.AsTypedObj(&pod)
-	if err != nil {
-		return ""
-	}
-
+func (s Corev1Pod) pendingDetailsReason(pod corev1.Pod) string {
 	statuses := append([]corev1.ContainerStatus{}, pod.Status.InitContainerStatuses...)
 	statuses = append(statuses, pod.Status.ContainerStatuses...)
 
