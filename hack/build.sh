b645918be88c91b79c9ca4b3bc76a1e34ff3cadd
Dmitriy Kalinin <cppforlife@gmail.com>
2019-12-19 13:00:20 -0500

    disable cgo and build binaries in a reproducible way

--- hack/build.sh
+++ hack/build.sh
@@ -2,6 +2,10 @@
 
 set -e -x -u
 
+# makes builds reproducible
+export CGO_ENABLED=0
+repro_flags="-ldflags=-buildid= -trimpath"
+
 go fmt ./cmd/... ./pkg/... ./test/...
 
 build_values_path="../../../${BUILD_VALUES:-./hack/build-values-default.yml}"
@@ -20,12 +24,12 @@ build_values_path="../../../${BUILD_VALUES:-./hack/build-values-default.yml}"
 )
 mv tmp/generated.go.txt pkg/kapp/website/generated.go
 
-go build -o kapp ./cmd/kapp/...
+go build $repro_flags -o kapp ./cmd/kapp/...
 ./kapp version
 
 # build aws lambda binary
 export GOOS=linux GOARCH=amd64
-go build -o ./tmp/main ./cmd/kapp-lambda-website/...
+go build $repro_flags -o ./tmp/main ./cmd/kapp-lambda-website/...
 (
 	cd tmp
 	chmod +x main
