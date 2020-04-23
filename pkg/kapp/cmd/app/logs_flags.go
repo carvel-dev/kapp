ba3a116b9e20f8e8ff9046b2bb7b3832946c1c78
Dmitriy Kalinin <cppforlife@gmail.com>
2020-04-23 12:13:28 -0400

    make --container-name help message more consistent with other flags

--- pkg/kapp/cmd/app/logs_flags.go
+++ pkg/kapp/cmd/app/logs_flags.go
@@ -18,9 +18,14 @@ type LogsFlags struct {
 func (s *LogsFlags) Set(cmd *cobra.Command) {
 	cmd.Flags().BoolVarP(&s.Follow, "follow", "f", false, "As new pods are added, new pod logs will be printed")
 	cmd.Flags().Int64Var(&s.Lines, "lines", 10, "Limit to number of lines (use -1 to remove limit)")
+
 	cmd.Flags().BoolVar(&s.ContainerTag, "container-tag", true, "Include container tag")
-	cmd.Flags().StringVarP(&s.PodName, "pod-name", "m", "", "Set pod name to filter logs (% acts as wildcard, e.g. 'app%')")
-	cmd.Flags().StringSliceVarP(&s.ContainerNames, "container-name", "c", nil, "Set container names to filter logs in the pod, separated by comma (% acts as wildcard, e.g. 'app%')")
+
+	cmd.Flags().StringVarP(&s.PodName, "pod-name", "m", "",
+		"Set pod name to filter logs (% acts as wildcard, e.g. 'app%')")
+
+	cmd.Flags().StringSliceVarP(&s.ContainerNames, "container-name", "c", nil,
+		"Set container name to filter logs (% acts as wildcard, e.g. 'app%') (can repeat)")
 }
 
 func (s *LogsFlags) PodLogOpts() (ctllogs.PodLogOpts, error) {
