98dde025bfa39c55e70ad1cd98f3b48501b6d22b
Dmitriy Kalinin <cppforlife@gmail.com>
2020-06-02 12:37:44 -0400

    add some more example dirs into ./hack/test-external.sh

--- hack/test-external.sh
+++ hack/test-external.sh
@@ -2,6 +2,12 @@
 
 set -e -x -u
 
+time kapp app-group deploy -y -g gitops -d examples/gitops/
+time kapp app-group delete -y -g gitops
+
+time kapp deploy -y -a istio -f examples/istio-v1.4.0/istio.yml
+time kapp delete -y -a istio
+
 time kapp deploy -y -a cert-manager -f examples/cert-manager-v0.15.1/
 time kapp delete -y -a cert-manager
 
