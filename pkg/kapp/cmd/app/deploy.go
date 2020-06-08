5d2a975c8e775db1d53630412602e4daf0fa4fd8
Brandon Kalinowski <brandon@brandonkalinowski.com>
2020-06-08 16:39:16 -0700

    formatting

--- pkg/kapp/cmd/app/deploy.go
+++ pkg/kapp/cmd/app/deploy.go
@@ -228,6 +228,7 @@ func (o *DeployOptions) newResources(
 	if err != nil {
 		return nil, ctlconf.Conf{}, nil, err
 	}
+
 	err = labeledResources.Prepare(newResources, conf.OwnershipLabelMods(),
 		conf.LabelScopingMods(), conf.AdditionalLabels())
 	if err != nil {
