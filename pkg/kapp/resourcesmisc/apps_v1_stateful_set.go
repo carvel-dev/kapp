f0d295df390ef0dafa389047ee1d03c300a4899c
Garrett Cheadle <gcheadle@vmware.com>
2020-07-01 16:23:59 -0700

    WIP Add waiting behavior for StatefulSet
    - Return success if updateStrategy is `OnDelete`. Do we want to report this to the user?
    
    Signed-off-by: Cari Dean <cdean@pivotal.io>
    Signed-off-by: Garrett Cheadle <gcheadle@vmware.com>

---
+++ pkg/kapp/resourcesmisc/apps_v1_stateful_set.go
@@ -0,0 +1,62 @@
+package resourcesmisc
+
+import (
+	"fmt"
+	ctlres "github.com/k14s/kapp/pkg/kapp/resources"
+	appsv1 "k8s.io/api/apps/v1"
+)
+
+type AppsV1StatefulSet struct {
+	resource     ctlres.Resource
+}
+
+func NewAppsV1StatefulSet(resource ctlres.Resource, associatedRs []ctlres.Resource) *AppsV1StatefulSet {
+	matcher := ctlres.APIVersionKindMatcher{
+		APIVersion: "apps/v1",
+		Kind:       "StatefulSet",
+	}
+	if matcher.Matches(resource) {
+		return &AppsV1StatefulSet{resource}
+	}
+	return nil
+}
+
+func (s AppsV1StatefulSet) IsDoneApplying() DoneApplyState {
+	statefulSet := appsv1.StatefulSet{}
+
+	err := s.resource.AsTypedObj(&statefulSet)
+	if err != nil {
+		return DoneApplyState{Done: true, Successful: false, Message: fmt.Sprintf("Error: Failed obj conversion: %s", err)}
+	}
+
+	updateStrategy := statefulSet.Spec.UpdateStrategy
+	if updateStrategy.Type == "OnDelete" {
+		return DoneApplyState{Done: true, Successful: true}
+	}
+
+	if statefulSet.Generation != statefulSet.Status.ObservedGeneration {
+		return DoneApplyState{Done: false, Message: fmt.Sprintf(
+			"Waiting for generation %d to be observed", statefulSet.Generation)}
+	}
+
+	// TODO: introduce logic for partition
+
+	if statefulSet.Status.ReadyReplicas < statefulSet.Status.Replicas ||
+			statefulSet.Status.UpdatedReplicas < statefulSet.Status.Replicas {
+		return DoneApplyState{Done: false, Message: fmt.Sprintf(
+			"Waiting for %d replicas to be updated and ready (currently %d updated and %d ready)",
+			  statefulSet.Status.Replicas - min(statefulSet.Status.UpdatedReplicas, statefulSet.Status.ReadyReplicas),
+			  statefulSet.Status.UpdatedReplicas,
+			  statefulSet.Status.ReadyReplicas,
+			)}
+	}
+
+	return DoneApplyState{Done: true, Successful: true}
+}
+
+func min(a, b int32) int32 {
+	if a < b {
+		return a
+	}
+	return b
+}
---
+++ pkg/kapp/resourcesmisc/apps_v1_stateful_set_test.go
@@ -0,0 +1,118 @@
+package resourcesmisc_test
+
+import (
+	"strings"
+	"testing"
+
+	ctlres "github.com/k14s/kapp/pkg/kapp/resources"
+	ctlresm "github.com/k14s/kapp/pkg/kapp/resourcesmisc"
+)
+
+func TestAppsV1StatefulSet(t *testing.T) {
+	configYAML := `
+apiVersion: apps/v1
+kind: StatefulSet
+metadata:
+  name: web
+  generation: 1
+spec:
+  selector:
+    matchLabels:
+      app: nginx # has to match .spec.template.metadata.labels
+  serviceName: "nginx"
+  replicas: 3 # by default is 1
+  template:
+    metadata:
+      labels:
+        app: nginx # has to match .spec.selector.matchLabels
+    spec:
+      containers:
+      - name: nginx
+        image: k8s.gcr.io/nginx-slim:0.8
+        ports:
+        - containerPort: 80
+          name: web
+status:
+  observedGeneration: 1
+  replicas: 3
+  readyReplicas: 2
+  updateRevision: "1"
+  currentRevision: "1"
+  updatedReplicas: 3
+  currentReplicas: 3
+`
+
+	state := buildStatefulSet(configYAML, t).IsDoneApplying()
+	expectedState := ctlresm.DoneApplyState{
+		Done:       false,
+		Successful: false,
+		Message:    "Waiting for 1 replicas to be updated and ready (currently 3 updated and 2 ready)",
+	}
+	if state != expectedState {
+		t.Fatalf("Found incorrect state: %#v", state)
+	}
+
+	configYAML = strings.Replace(configYAML, "readyReplicas: 2", "readyReplicas: 3", -1)
+
+	state = buildStatefulSet(configYAML, t).IsDoneApplying()
+	if state != (ctlresm.DoneApplyState{Done: true, Successful: true, Message: ""}) {
+		t.Fatalf("Found incorrect state: %#v", state)
+	}
+}
+
+func TestAppsV1StatefulSetOnDelete(t *testing.T) {
+	configYAML := `
+apiVersion: apps/v1
+kind: StatefulSet
+metadata:
+  name: web
+  generation: 1
+spec:
+  selector:
+    matchLabels:
+      app: nginx # has to match .spec.template.metadata.labels
+  serviceName: "nginx"
+  replicas: 3 # by default is 1
+  template:
+    metadata:
+      labels:
+        app: nginx # has to match .spec.selector.matchLabels
+    spec:
+      containers:
+      - name: nginx
+        image: k8s.gcr.io/nginx-slim:0.8
+        ports:
+        - containerPort: 80
+          name: web
+  updateStrategy:
+    type: OnDelete
+status:
+  observedGeneration: 1
+  replicas: 3
+  readyReplicas: 2
+  updateRevision: "1"
+  currentRevision: "1"
+  updatedReplicas: 3
+  currentReplicas: 3
+`
+
+	state := buildStatefulSet(configYAML, t).IsDoneApplying()
+	expectedState := ctlresm.DoneApplyState{
+		Done:       true,
+		Successful: true,
+		Message:    "",
+	}
+	if state != expectedState {
+		t.Fatalf("Found incorrect state: %#v", state)
+	}
+
+}
+
+func buildStatefulSet(resourcesBs string, t *testing.T) *ctlresm.AppsV1StatefulSet {
+	newResources, err := ctlres.NewFileResource(ctlres.NewBytesSource([]byte(resourcesBs))).Resources()
+	if err != nil {
+		t.Fatalf("Expected resources to parse")
+	}
+
+	return ctlresm.NewAppsV1StatefulSet(newResources[0], nil)
+}
