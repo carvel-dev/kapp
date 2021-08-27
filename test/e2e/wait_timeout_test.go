a7a7bcf21c03755e0b37166fb034e8de3354a54b
sethiyash <yashsethiya97@gmail.com>
2021-08-27 19:05:40 +0530

    e2e test cases updated (#208)

--- test/e2e/wait_timeout_test.go
+++ test/e2e/wait_timeout_test.go
@@ -14,6 +14,25 @@ func TestWaitTimeout(t *testing.T) {
 	kapp := Kapp{t, env.Namespace, env.KappBinaryPath, logger}
 
 	yaml1 := `
+ apiVersion: batch/v1 
+ kind: Job 
+ metadata: 
+   name: successful-job 
+   namespace: __ns__ 
+   annotations: 
+     kapp.k14s.io/update-strategy: always-replace 
+     kapp.k14s.io/change-group: job
+ spec: 
+   template: 
+     metadata: 
+       name: successful-job 
+     spec: 
+       containers: 
+       - name: successful-job 
+         image: busybox 
+         command: ["/bin/sh", "-c", "sleep 5"] 
+       restartPolicy: Never
+---
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -33,46 +52,6 @@ spec:
           env:
             - name: HELLO_MSG
               value: stranger
----
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: simple-app2
-spec:
-  selector:
-    matchLabels:
-      simple-app2: ""
-  template:
-    metadata:
-      labels:
-        simple-app2: ""
-    spec:
-      containers:
-        - name: simple-app2
-          image: docker.io/dkalinin/k8s-simple-app@sha256:4c8b96d4fffdfae29258d94a22ae4ad1fe36139d47288b8960d9958d1e63a9d0
-          env:
-            - name: HELLO_MSG
-              value: stranger
----
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: simple-app3
-spec:
-  selector:
-    matchLabels:
-      simple-app3: ""
-  template:
-    metadata:
-      labels:
-        simple-app3: ""
-    spec:
-      containers:
-        - name: simple-app3
-          image: docker.io/dkalinin/k8s-simple-app@sha256:4c8b96d4fffdfae29258d94a22ae4ad1fe36139d47288b8960d9958d1e63a9d0
-          env:
-            - name: HELLO_MSG
-              value: stranger
 `
 
 	name := "test-wait-timeout"
@@ -83,25 +62,49 @@ spec:
 	cleanUp()
 	defer cleanUp()
 
-	logger.Section("Resource wait timeout", func() {
+	logger.Section("Resource timed out waiting", func() {
 		_, err := kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", name, "--wait-timeout",
-			"2m", "--wait-resource-timeout", "2ms", "--json"},
+			"2m", "--wait-resource-timeout", "3s", "--json"},
 			RunOpts{IntoNs: true, AllowError: true, StdinReader: strings.NewReader(yaml1)})
 
-		if !strings.Contains(err.Error(), "Resource timed out waiting after 2ms") {
+		if !strings.Contains(err.Error(), "Resource timed out waiting after 3s") {
 			t.Fatalf("Expected to see timed out, but did not: '%s'", err.Error())
 		}
 	})
 
 	cleanUp()
 
-	logger.Section("Global wait timeout", func() {
+	logger.Section("Resource reconciled successfully before timeout", func() {
 		_, err := kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", name, "--wait-timeout",
-			"2ms", "--wait-resource-timeout", "2m", "--json"},
+			"2m", "--wait-resource-timeout", "15s", "--json"},
 			RunOpts{IntoNs: true, AllowError: true, StdinReader: strings.NewReader(yaml1)})
 
-		if !strings.Contains(err.Error(), "kapp: Error: Timed out waiting after 2ms") {
+		if err != nil {
+			t.Fatalf("Expected to be successful without resource timeout: '%s'", err)
+		}
+	})
+
+	cleanUp()
+
+	logger.Section("Global timeout waiting", func() {
+		_, err := kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", name, "--wait-timeout",
+			"2s", "--wait-resource-timeout", "10s", "--json"},
+			RunOpts{IntoNs: true, AllowError: true, StdinReader: strings.NewReader(yaml1)})
+
+		if !strings.Contains(err.Error(), "kapp: Error: Timed out waiting after 2s") {
 			t.Fatalf("Expected to see timed out, but did not: '%s'", err.Error())
+		}
+	})
+
+	cleanUp()
+
+	logger.Section("Resource reconciled successfully before timeout", func() {
+		_, err := kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", name, "--wait-timeout",
+			"1m", "--wait-resource-timeout", "15s", "--json"},
+			RunOpts{IntoNs: true, AllowError: true, StdinReader: strings.NewReader(yaml1)})
+
+		if err != nil {
+			t.Fatalf("Expected to be successful without global timeout: '%s'", err)
 		}
 	})
 }
