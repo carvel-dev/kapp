f913b8206ed25120ca32349082956164f0add4cd
Soumik Majumder <soumikm@vmware.com>
2021-09-07 14:32:34 +0530

    Updated tests to check for appearance of warnings when resources are applied

--- test/e2e/warnings_test.go
+++ test/e2e/warnings_test.go
@@ -4,12 +4,18 @@
 package e2e
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 	"testing"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+)
+
+const (
+	yellowColor = "\u001b[33;1m"
+	resetColor  = "\u001b[0m"
 )
 
 func TestWarningsFlag(t *testing.T) {
@@ -24,15 +30,18 @@ func TestWarningsFlag(t *testing.T) {
 	logger := Logger{}
 	kapp := Kapp{t, env.Namespace, env.KappBinaryPath, Logger{}}
 	crdName := "test-no-warnings-crd"
-	crName := "test-no-warnings-cr"
+	crName1 := "test-no-warnings-cr1"
+	crName2 := "test-no-warnings-cr2"
 	cleanUp := func() {
-		kapp.Run([]string{"delete", "-a", crName})
+		kapp.Run([]string{"delete", "-a", crName1})
+		kapp.Run([]string{"delete", "-a", crName2})
 		kapp.Run([]string{"delete", "-a", crdName})
 	}
 
 	cleanUp()
 	defer cleanUp()
 	customWarning := "example.com/v1alpha1 CronTab is deprecated; use example.com/v1 CronTab"
+	outputWarning := fmt.Sprintf("\n%sWarning:%s %s", yellowColor, resetColor, customWarning)
 
 	crdYaml := `
 apiVersion: apiextensions.k8s.io/v1
@@ -72,27 +81,62 @@ spec:
   image: my-awesome-cron-image
 `
 
+	expectedOutputTemplate := `
+Target cluster 'https://127.0.0.1:60817' (nodes: minikube)
+
+Changes
+
+Namespace  Name  Kind     Conds.  Age  Op      Op st.  Wait to    Rs  Ri  
+kapp-test  <cr-name>  CronTab  -       -    create  -       reconcile  -   -  
+
+Op:      1 create, 0 delete, 0 update, 0 noop
+Wait to: 1 reconcile, 0 delete, 0 noop
+
+<replaced>: ---- applying 1 changes [0/1 done] ----<output-warning>
+<replaced>: create crontab/<cr-name> (stable.example.com/v1alpha1) namespace: kapp-test
+<replaced>: ---- waiting on 1 changes [0/1 done] ----
+<replaced>: ok: reconcile crontab/<cr-name> (stable.example.com/v1alpha1) namespace: kapp-test
+<replaced>: ---- applying complete [1/1 done] ----
+<replaced>: ---- waiting complete [1/1 done] ----
+
+Succeeded`
+
 	logger.Section("deploying crd with deprecated version", func() {
 		yaml := strings.Replace(crdYaml, "<customWarning>", customWarning, 1)
 		kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", crdName},
 			RunOpts{StdinReader: strings.NewReader(yaml)})
 	})
+
 	logger.Section("deploying without --warnings flag", func() {
 		yaml := strings.Replace(crYaml, "<cr-name>", "cr-1", 1)
-		out, _ := kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", crName},
+
+		out, _ := kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", crName1},
 			RunOpts{StdinReader: strings.NewReader(yaml)})
 
-		if !strings.Contains(out, customWarning) {
-			t.Fatalf("Expected warning %s, but didn't get", customWarning)
+		expectedOutput := strings.Replace(expectedOutputTemplate, "<cr-name>", "cr-1", 3)
+
+		out = strings.TrimSpace(replaceTarget(replaceSpaces(replaceTs(out))))
+		expectedOutput = strings.Replace(expectedOutput, "<output-warning>", outputWarning, 1)
+		expectedOutput = strings.TrimSpace(replaceTarget(replaceSpaces(expectedOutput)))
+
+		if expectedOutput != out {
+			t.Fatalf("Expected output with warning >>%s<<, but got >>%s<<\n", expectedOutput, out)
 		}
 	})
+
 	logger.Section("deploying with --warnings flag", func() {
 		yaml := strings.Replace(crYaml, "<cr-name>", "cr-2", 1)
-		out, _ := kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", crName, "--warnings=false"},
+
+		out, _ := kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", crName2, "--warnings=false"},
 			RunOpts{StdinReader: strings.NewReader(yaml)})
 
-		if strings.Contains(out, customWarning) {
-			t.Fatalf("Expected no warning, but got %s", customWarning)
+		expectedOutput := strings.Replace(expectedOutputTemplate, "<cr-name>", "cr-2", 3)
+
+		out = strings.TrimSpace(replaceTarget(replaceSpaces(replaceTs(out))))
+		expectedOutput = strings.Replace(expectedOutput, "<output-warning>", "", 1)
+		expectedOutput = strings.TrimSpace(replaceTarget(replaceSpaces(expectedOutput)))
+		if expectedOutput != out {
+			t.Fatalf("Expected output without warning >>%s<< but got >>%s<<\n", expectedOutput, out)
 		}
 	})
 }
