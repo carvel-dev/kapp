fd0f7902573f175ae6e23d2d08f3c6e456e0e492
Maxim Ivanov <hi+github@yamlcoder.me>
2021-12-10 13:02:08 +0000

    Adds e2e test for simple server side apply merge case

---
+++ test/e2e/update_server_side_test.go
@@ -0,0 +1,128 @@
+// Copyright 2020 VMware, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+package e2e
+
+import (
+	"github.com/ghodss/yaml"
+	corev1 "k8s.io/api/core/v1"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSSAUpdateSimple(t *testing.T) {
+	env := BuildEnv(t)
+	logger := Logger{}
+	kapp := Kapp{t, env.Namespace, env.KappBinaryPath, logger}
+
+	yaml1 := `
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: redis-primary
+spec:
+  ports:
+  - port: 6380
+    name: p0
+  selector:
+    app: redis
+`
+
+	kubectlChange := `
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: redis-primary
+spec:
+  ports:
+  - port: 6381
+    name: p1
+  selector:
+    app: redis
+`
+
+	yaml2 := `
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: redis-primary
+spec:
+  ports:
+  - port: 6380
+    name: p0
+  - port: 6382
+    name: p2
+  selector:
+    app: redis
+`
+
+	yamlExpected, _ := yaml.YAMLToJSON([]byte(`
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: redis-primary
+spec:
+  ports:
+  - port: 6380
+    name: p0
+  - port: 6381
+    name: p1
+  - port: 6382
+    name: p2
+  selector:
+    app: redis
+`))
+
+	name := strings.ToLower(t.Name())
+	cleanUp := func() {
+		//kapp.Run([]string{"delete", "-a", name})
+	}
+
+	kubectl := Kubectl{t, env.Namespace, logger}
+
+	cleanUp()
+	defer cleanUp()
+
+	logger.Section("deploy basic service", func() {
+		kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", name},
+			RunOpts{IntoNs: true, StdinReader: strings.NewReader(yaml1)})
+	})
+
+	logger.Section("edit resource with kubectl outside of kapp", func() {
+		_, err := kubectl.RunWithOpts([]string{"apply", "--validate=false", "--server-side", "-f", "-"},
+			RunOpts{IntoNs: true, StdinReader: strings.NewReader(kubectlChange)},
+		)
+		require.NoError(t, err)
+	})
+
+	logger.Section("deploy updated service", func() {
+		kapp.RunWithOpts([]string{"deploy", "--yes", "-f", "-", "-a", name},
+			RunOpts{IntoNs: true, StdinReader: strings.NewReader(yaml2)})
+	})
+
+	inClusterObj := corev1.Service{}
+
+	err := kubectl.RunWithOptsIntoJson([]string{"get", "svc", "redis-primary"},
+		RunOpts{IntoNs: true}, &inClusterObj)
+	require.NoError(t, err)
+
+	tmpFile := newTmpFile(string(yamlExpected), t)
+	defer os.Remove(tmpFile.Name())
+
+	expectedObj := corev1.Service{}
+
+	// Patch dry run returns merged object with all patch-file fields present
+	err = kubectl.RunWithOptsIntoJson([]string{"patch", "svc", "redis-primary", "--patch-file", tmpFile.Name(), "--dry-run=client"},
+		RunOpts{IntoNs: true}, &expectedObj)
+	require.NoError(t, err)
+
+	require.Equal(t, expectedObj, inClusterObj)
+
+}
