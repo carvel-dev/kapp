3c4361844c771a9920e6388c8f7a1be1f8d9162e
Praveen Rewar <8457124+praveenrewar@users.noreply.github.com>
2022-02-15 11:40:32 +0530

    Add newer versions/releases of tools in examples (#420)
    * Add cert-manager-v1.6.1 to examples
    
    * Update test-external.sh to use cert-manager-v1.6.1
    
    * Add knative-v1.1.0 to examples
    
    * Update test-externals.sh to use knative-v1.1.0
    
    * Add istio-v1.12.2 to examples
    
    * Update test-external.sh to use istio-v1.12.2
    
    * Add gatekeeper-v3.7.0 to examples
    
    * Update test-external.sh to test gatekeeper
    
    * Add pinniped-v0.13.0 to examples
    
    * Add pinniped test to test-external.sh
    
    * Add minikube tunnel to test-external.sh

---
+++ examples/cert-manager-v1.6.1/README.md
@@ -0,0 +1,5 @@
+Downloaded via
+
+```bash
+ytt --ignore-unknown-comments -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.yaml > config.yaml
+```
--- hack/test-external.sh
+++ hack/test-external.sh
@@ -2,19 +2,28 @@
 
 set -e -x -u
 
+minikube tunnel &> /dev/null &
+
 time kapp app-group deploy -y -g gitops -d examples/gitops/
 time kapp app-group delete -y -g gitops
 
-time kapp deploy -y -a istio -f examples/istio-v1.4.0/istio.yml
+time kapp deploy -y -a istio -f examples/istio-v1.12.2/
 time kapp delete -y -a istio
 
-time kapp deploy -y -a cert-manager -f examples/cert-manager-v0.15.1/
+time kapp deploy -y -a cert-manager -f examples/cert-manager-v1.6.1/
 time kapp delete -y -a cert-manager
 
-time kapp deploy -y -a knative -f examples/knative-v0.15.0/
+time kapp deploy -y -a knative -f examples/knative-v1.1.0/
 time kapp delete -y -a knative
 
-time kapp deploy -y -a cf -f examples/cf-for-k8s-v0.2.0-custom/
-time kapp delete -y -a cf
+# TODO Add cf-for-k8s-v5.4.3
+
+time kapp deploy -y -a gk -f examples/gatekeeper-v3.7.0/config.yml
+time kapp delete -y -a gk
+
+time kapp deploy -y -a pinniped -f examples/pinniped-v0.13.0/
+time kapp delete -y -a pinniped
+
+pkill -9 'minikube'
 
 echo EXTERNAL SUCCESS
