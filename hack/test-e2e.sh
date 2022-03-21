10474439207bd37c462a84cab910d80a2dc6ca41
rohitagg2020 <rohit.aggarwal2020@gmail.com>
2022-03-21 16:03:12 +0530

    Update test-e2e.sh

--- hack/test-e2e.sh
+++ hack/test-e2e.sh
@@ -5,8 +5,7 @@ set -e -x -u
 go clean -testcache
 
 export KAPP_BINARY_PATH="${KAPP_BINARY_PATH:-$PWD/kapp}"
-export KAPP_E2E_NAMESPACE="kapp-test"
-kubectl create ns $KAPP_E2E_NAMESPACE
+
 go test ./test/e2e/ -timeout 60m -test.v $@
 
 echo E2E SUCCESS
