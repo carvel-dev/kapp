daf2ff96e219c98f1799f5114a0bf701a5c55b0c
Joe Kimmel <jkimmel@vmware.com>
2022-04-11 11:02:44 -0700

    e2e test: error message on missing kapp e2e ns suggests how to fix

--- test/e2e/env.go
+++ test/e2e/env.go
@@ -34,7 +34,7 @@ func (e Env) Validate(t *testing.T) {
 	errStrs := []string{}
 
 	if len(e.Namespace) == 0 {
-		errStrs = append(errStrs, "Expected Namespace to be non-empty")
+		errStrs = append(errStrs, "Expected Namespace to be non-empty (Hint: set ENV var KAPP_E2E_NAMESPACE")
 	}
 
 	require.Lenf(t, errStrs, 0, "%s", strings.Join(errStrs, "\n"))
