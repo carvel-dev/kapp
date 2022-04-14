5b2a852bd7a6341b1e780762f2535c6a0ae2f5dc
Joe Kimmel <jkimmel@vmware.com>
2022-04-14 08:58:00 -0700

    test cleanup

--- test/e2e/create_fallback_on_noop_test.go
+++ test/e2e/create_fallback_on_noop_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 VMware, Inc.
+// Copyright 2022 VMware, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
 package e2e
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCreateFallbackOnNoop(t *testing.T) {
@@ -15,26 +16,16 @@ func TestCreateFallbackOnNoop(t *testing.T) {
 	logger := Logger{}
 	kapp := Kapp{t, env.Namespace, env.KappBinaryPath, logger}
 
-	objNs := env.Namespace + "-create-fallback-on-noop"
-	nsYaml := strings.Replace(`
----
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: __ns__
-`, "__ns__", objNs, -1)
-	objYaml := strings.Replace(`
+	objYaml := `
 ---
 apiVersion: v1
 kind: ConfigMap
 metadata:
   name: precious-resource
-  namespace: __ns__
   annotations:
     kapp.k14s.io/create-strategy: "fallback-on-update-or-noop"
 data: {"importantFact":"true","releasedAt":null}
-`, "__ns__", objNs, -1)
-
+`
 	rebaseRule := `
 ---
 apiVersion: kapp.k14s.io/v1alpha1
@@ -55,9 +46,8 @@ rebaseRules:
   resourceMatchers:
   - apiVersionKindMatcher: {apiVersion: v1, kind: ConfigMap}
 `
-
 	name := "test-create-fallback-on-noop"
-	name2 := "test-2"
+	name2 := "test-create-fallback-on-noop2"
 	cleanUp := func() {
 		kapp.Run([]string{"delete", "-a", name})
 		kapp.Run([]string{"delete", "-a", name2})
@@ -66,22 +56,18 @@ rebaseRules:
 	cleanUp()
 	defer cleanUp()
 
-	logger.Section("deploy initial ns and package", func() {
-		_, err := kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", name, "-y"},
-			RunOpts{AllowError: false, StdinReader: strings.NewReader(nsYaml + objYaml)})
-		assert.NoError(t, err)
-	})
+	logger.Section("deploy initial app and assert it has its precious", func() {
+		kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", name, "-y"},
+			RunOpts{StdinReader: strings.NewReader(objYaml)})
 
-	logger.Section("assert theres a configmap that belongs to the app", func() {
 		out := kapp.Run([]string{"inspect", "-a", name})
-		assert.Contains(t, out, "precious-resource")
+		require.Contains(t, out, "precious-resource")
 	})
 
 	logger.Section("deploy a second app with noop-strategy annotation", func() {
-		_, err := kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", name2,
+		kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", name2,
 			"--existing-non-labeled-resources-check=false", "--dangerous-override-ownership-of-existing-resources", "-y", "--json"},
-			RunOpts{AllowError: false, StdinReader: strings.NewReader(objYaml + rebaseRule)})
-		assert.NoError(t, err)
+			RunOpts{StdinReader: strings.NewReader(objYaml + rebaseRule)})
 	})
 
 	logger.Section("assert the configmap still belongs to the first app, not the second", func() {
@@ -92,10 +78,9 @@ rebaseRules:
 	})
 
 	logger.Section("redeploy the second app without the rebase rule and it steals ownership of the configmap", func() {
-		_, err := kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", name2,
+		kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", name2,
 			"--existing-non-labeled-resources-check=false", "--dangerous-override-ownership-of-existing-resources", "-y", "--json"},
-			RunOpts{AllowError: false, StdinReader: strings.NewReader(objYaml)})
-		assert.NoError(t, err)
+			RunOpts{StdinReader: strings.NewReader(objYaml)})
 
 		out := kapp.Run([]string{"inspect", "-a", name2})
 		assert.Contains(t, out, "precious-resource")
