f279ff392d435f04fdc155ef2bd5cb02cdfd84a1
Neil Hickey <nhickey@vmware.com>
2022-05-09 15:49:35 -0600

    Add migration_flags.go
    Signed-off-by: Neil Hickey <nhickey@vmware.com>

---
+++ pkg/kapp/cmd/app/migration_flags.go
@@ -0,0 +1,16 @@
+// Copyright 2020 VMware, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+package app
+
+import (
+	"github.com/spf13/cobra"
+)
+
+type MigrationFlags struct {
+	prevAppName string
+}
+
+func (s *MigrationFlags) Set(cmd *cobra.Command) {
+	cmd.Flags().StringVar(&s.prevAppName, "prev-app", "", "Name of old app incase of migration")
+}
