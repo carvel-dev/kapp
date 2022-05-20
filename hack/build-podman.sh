957445e48ccd181ec5ff16e69cc541aa55ed2ba9
Christoph "criztovyl" Schulz <git@criztovyl.space>
2022-05-20 11:08:03 +0200

    Add script shortcuts to podman scripts

--- hack/build-podman.sh
+++ hack/build-podman.sh
@@ -2,4 +2,4 @@
 
 cd $(dirname ${BASH_SOURCE[0]})
 
-./podman.sh hack/build.sh
+./podman.sh build
--- hack/podman.sh
+++ hack/podman.sh
@@ -2,4 +2,8 @@
 
 cd $(dirname ${BASH_SOURCE[0]})
 
-podman run --rm -v gopath:/go -v gobuild:/root/.cache/go-build -v $(realpath $PWD/..):/mnt -w /mnt docker.io/golang $1
+action=$1
+
+[ -x "$action.sh" ] && action=hack/$action.sh
+
+podman run --rm -v gopath:/go -v gobuild:/root/.cache/go-build -v $(realpath $PWD/..):/mnt -w /mnt docker.io/golang $action
