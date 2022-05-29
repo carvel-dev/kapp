9a74bfda025fbe2567f07cdc398491e4b14bfbd2
Christoph "criztovyl" Schulz <git@criztovyl.space>
2022-05-29 10:49:11 +0200

    Podman interactive
    e.g. for dlv debugging

--- hack/podman.sh
+++ hack/podman.sh
@@ -4,6 +4,10 @@ cd $(dirname ${BASH_SOURCE[0]})
 
 action=$1
 
-[ -x "$action.sh" ] && action=hack/$action.sh
+if [ "$action" ]; then
+    [ -x "$action.sh" ] && action=hack/$action.sh
+else
+    interactive="-it"
+fi
 
-podman run --rm -v gopath:/go -v gobuild:/root/.cache/go-build -v $(realpath $PWD/..):/mnt -w /mnt docker.io/golang $action
+podman run --rm -v gopath:/go -v gobuild:/root/.cache/go-build -v $(realpath $PWD/..):/mnt -w /mnt $interactive --cap-add SYS_PTRACE docker.io/golang $action
