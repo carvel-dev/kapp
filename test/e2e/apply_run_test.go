b063fc1f2f9941572e4ee917a5c4e3f877dd5057
Kumari Tanushree <ktanushree@ktanushree-a01.vmware.com>
2022-06-09 19:01:18 +0530

    488: updated e2e test case changes

--- test/e2e/apply_run_test.go
+++ test/e2e/apply_run_test.go
@@ -1,7 +1,6 @@
 package e2e
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 
@@ -18,10 +17,15 @@ apiVersion: v1
 kind: ConfigMap
 metadata:
   name: simple-cm
-  annotations:
-    kapp.k14s.io/delete-strategy: "orphan"
 data:
-  hello_msg: good-morning-banaglore
+  hello_msg: good-morning-bangalore
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: simple-cm1
+data:
+  hello_msg: hello
 `
 	name := "test-apply-run"
 	cleanUp := func() {
@@ -38,18 +42,107 @@ data:
 ---
 apiVersion: v1
 data:
-  hello_msg: good-morning-banaglore
+  hello_msg: good-morning-bangalore
 kind: ConfigMap
 metadata:
-  annotations:
-    kapp.k14s.io/delete-strategy: orphan
   labels:
+    kapp.k14s.io/app: "1654768619446772000"
+    kapp.k14s.io/association: v1.aa6ba70e7f14b3140de8009fda0a6fad
+  name: simple-cm
+  namespace: kapp-test
+# add: configmap/simple-cm1 (v1) namespace: kapp-test
+---
+apiVersion: v1
+data:
+  hello_msg: hello
+kind: ConfigMap
+metadata:
+  labels:
+    kapp.k14s.io/app: "1654768619446772000"
+    kapp.k14s.io/association: v1.df359155db7824da8b7d86ec097a40cf
+  name: simple-cm1
+  namespace: kapp-test
+
+Succeeded
 `
 		out = strings.TrimSpace(replaceTarget(replaceSpaces(replaceTs(out))))
-		fmt.Printf("out: %s", out)
+		// filtering value for label "kapp.k14s.io/app:" and updating with hardcode value
+		if out != "" && len(strings.Split(out, "kapp.k14s.io/app: ")) > 2 {
+			repValue1 := strings.Split((strings.Split(out, "kapp.k14s.io/app: ")[1]), "\n")[0]
+			out = strings.Replace(out, repValue1, `"1654768619446772000"`, 1)
+			repValue2 := strings.Split((strings.Split(out, "kapp.k14s.io/app: ")[2]), "\n")[0]
+			out = strings.Replace(out, repValue2, `"1654768619446772000"`, 1)
+		}
+
 		expectedOutput = strings.TrimSpace(replaceSpaces(expectedOutput))
-		//require.Equal(t, expectedOutput, out)
 		require.Contains(t, out, expectedOutput, "output does not match")
+
+		_, _ = kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", name},
+			RunOpts{IntoNs: true, StdinReader: strings.NewReader(yaml)})
+	})
+
+	yaml1 := `
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: simple-cm
+data:
+  hello_msg: good-morning
+`
+
+	logger.Section("update configmap simple-cm and remove configmap simple-cm1", func() {
+		out, _ := kapp.RunWithOpts([]string{"deploy", "-f", "-", "-a", name, "--apply-run"},
+			RunOpts{IntoNs: true, StdinReader: strings.NewReader(yaml1)})
+		expectedOutput := `
+# update: configmap/simple-cm (v1) namespace: kapp-test
+---
+apiVersion: v1
+data:
+  hello_msg: good-morning
+kind: ConfigMap
+metadata:
+  creationTimestamp: "2022-05-11T15:27:14Z"
+  labels:
+    kapp.k14s.io/app: "1654768619446772000"
+    kapp.k14s.io/association: v1.aa6ba70e7f14b3140de8009fda0a6fad
+  name: simple-cm
+  namespace: kapp-test
+  resourceVersion: "201881"
+  uid: ee57a857-1873-4c58-b021-8cc15bf4a3e6
+# delete: configmap/simple-cm1 (v1) namespace: kapp-test
+
+Succeeded
+`
+
+		// filtering value for label "kapp.k14s.io/app:" and replacing with hardcode value
+		if len(strings.Split(out, "kapp.k14s.io/app: ")) > 1 {
+			repValue := strings.Split((strings.Split(out, "kapp.k14s.io/app: ")[1]), "\n")[0]
+			out = strings.Replace(out, repValue, `"1654768619446772000"`, 1)
+		}
+
+		// filtering value for label "creationTimestamp:" and replacing with hardcode value
+		if len(strings.Split(out, "creationTimestamp: ")) > 1 {
+			repValue := strings.Split((strings.Split(out, "creationTimestamp: ")[1]), "\n")[0]
+			out = strings.Replace(out, repValue, `"2022-05-11T15:27:14Z"`, 1)
+		}
+
+		// filtering value for label "resourceVersion:" and replacing with hardcode value
+		if len(strings.Split(out, "resourceVersion: ")) > 1 {
+			repValue := strings.Split((strings.Split(out, "resourceVersion: ")[1]), "\n")[0]
+			out = strings.Replace(out, repValue, `"201881"`, 1)
+		}
+
+		// filtering value for label "uid:" and replacing with hardcode value
+		if len(strings.Split(out, "uid: ")) > 1 {
+			repValue := strings.Split((strings.Split(out, "uid: ")[1]), "\n")[0]
+			out = strings.Replace(out, repValue, "ee57a857-1873-4c58-b021-8cc15bf4a3e6", 1)
+		}
+		out = strings.TrimSpace(replaceTarget(replaceSpaces(replaceTs(out))))
+
+		expectedOutput = strings.TrimSpace(replaceSpaces(expectedOutput))
+		require.Contains(t, out, expectedOutput, "output does not match")
+
 	})
 
 }
