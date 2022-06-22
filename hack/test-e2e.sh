7c686fa37a5356d3db826ef832f70ad06fa28989
Víctor Martínez Bevià <6623606+vicmarbev@users.noreply.github.com>
2022-06-22 14:07:16 +0200

    Set default e2e namespace and create it if needed (#539)
    * Set default e2e namespace and create it if needed
    
    Signed-off-by: Víctor Martínez Bevià <vicmarbev@gmail.com>
    
    * Remove failing because unset variables. Add failure if one of the commands in the pipe fails.

--- hack/test-e2e.sh
+++ hack/test-e2e.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-set -e -x -u
+set -e -x -o pipefail
 
 go clean -testcache
 
 export KAPP_BINARY_PATH="${KAPP_BINARY_PATH:-$PWD/kapp}"
+
+if [ -z "$KAPP_E2E_NAMESPACE" ]; then
+    echo "setting e2e namespace to: kapp-test";
+    export KAPP_E2E_NAMESPACE="kapp-test"
+fi
+# create ns if not exists because the `apply -f -` won't complain on a no-op if the ns already exists.
+kubectl create ns $KAPP_E2E_NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
 
 go test ./test/e2e/ -timeout 60m -test.v $@
 
