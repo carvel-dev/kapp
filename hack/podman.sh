6b435dc9fe79355e5cf864a40d1eec56b85ab15d
Christoph "criztovyl" Schulz <git@criztovyl.space>
2022-08-29 17:00:18 +0200

    Fix persisting podman interactive history

--- hack/podman.sh
+++ hack/podman.sh
@@ -7,10 +7,15 @@ action=$1
 if [ "$action" ]; then
     [ -x "$action.sh" ] && action=hack/$action.sh
 else
-    interactive="-it -v $PWD/$histfile:/root/$histfile"
-
     histfile=.bash_history
     [ -f $histfile ] || >$histfile
+
+    interactive="-it -v $PWD/$histfile:/root/$histfile"
+
+    dlv_conf=.dlv
+    [ -d $dlv_conf ] || mkdir $dlv_conf
+
+    interactive="$interactive -v $PWD/$dlv_conf:/root/.config/dlv"
 fi
 
 # SYS_PTRACE for dlv
@@ -20,7 +25,8 @@ podman run --rm \
     -v gobuild:/root/.cache/go-build \
     -v $HOME/.kube:/root/.kube \
     -v $HOME/.minikube:$HOME/.minikube \
-    -v $(realpath $PWD/..):/mnt -w /mnt $interactive \
+    -v $(realpath $PWD/..):/mnt -w /mnt \
+    $interactive \
     --cap-add SYS_PTRACE \
     --network host \
     docker.io/golang $action
