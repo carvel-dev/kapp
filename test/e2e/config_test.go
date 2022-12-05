b0cad86dda1ac8111dc003889b1173190d832e74
rohitagg2020 <rohit.aggarwal2020@gmail.com>
2022-12-05 11:43:03 +0530

    Fixing test case and updating go, ubuntu version for k8s-1.16

--- test/e2e/config_test.go
+++ test/e2e/config_test.go
@@ -127,6 +127,12 @@ data:
 }
 
 func TestYttRebaseRule_ServiceAccountRebaseTokenSecret(t *testing.T) {
+	minorVersion, err := getServerMinorVersion()
+	require.NoErrorf(t, err, "Error getting k8s server minor version")
+
+	if minorVersion >= 24 {
+		t.Skip("Automatic creation of service account token is turned off in k8s v1.24.0+")
+	}
 	env := BuildEnv(t)
 	logger := Logger{}
 	kapp := Kapp{t, env.Namespace, env.KappBinaryPath, logger}
