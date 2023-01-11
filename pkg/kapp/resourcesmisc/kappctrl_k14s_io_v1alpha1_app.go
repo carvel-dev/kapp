81c0e975d2c5450b5c274c850cd98a8a5754bef8
Soumik Majumder <soumikm@vmware.com>
2023-01-11 15:09:14 +0530

    Update logic handling failures for kc resources: pkgi, pkgrs and apps

--- pkg/kapp/resourcesmisc/kappctrl_k14s_io_v1alpha1_app.go
+++ pkg/kapp/resourcesmisc/kappctrl_k14s_io_v1alpha1_app.go
@@ -51,12 +51,20 @@ func (s KappctrlK14sIoV1alpha1App) IsDoneApplying() DoneApplyState {
 			return DoneApplyState{Done: false, Message: "Reconciling"}
 
 		case cond.Type == kcv1alpha1.ReconcileFailed && cond.Status == corev1.ConditionTrue:
+			errorMsg := app.Status.UsefulErrorMessage
+			if errorMsg == "" {
+				errorMsg = cond.Message
+			}
 			return DoneApplyState{Done: true, Successful: false, Message: fmt.Sprintf(
-				"Reconcile failed: %s (message: %s)", cond.Reason, cond.Message)}
+				"Reconcile failed: %s (message: %s)", cond.Reason, errorMsg)}
 
 		case cond.Type == kcv1alpha1.DeleteFailed && cond.Status == corev1.ConditionTrue:
+			errorMsg := app.Status.UsefulErrorMessage
+			if errorMsg == "" {
+				errorMsg = cond.Message
+			}
 			return DoneApplyState{Done: true, Successful: false, Message: fmt.Sprintf(
-				"Delete failed: %s (message: %s)", cond.Reason, cond.Message)}
+				"Delete failed: %s (message: %s)", cond.Reason, errorMsg)}
 		}
 	}
 
--- pkg/kapp/resourcesmisc/packaging_carvel_dev_v1alpha1_packageinstall.go
+++ pkg/kapp/resourcesmisc/packaging_carvel_dev_v1alpha1_packageinstall.go
@@ -52,12 +52,20 @@ func (s PackagingCarvelDevV1alpha1PackageInstall) IsDoneApplying() DoneApplyStat
 			return DoneApplyState{Done: false, Message: "Reconciling"}
 
 		case cond.Type == appv1alpha1.ReconcileFailed && cond.Status == corev1.ConditionTrue:
+			errorMsg := pkgInstall.Status.UsefulErrorMessage
+			if errorMsg == "" {
+				errorMsg = cond.Message
+			}
 			return DoneApplyState{Done: true, Successful: false, Message: fmt.Sprintf(
-				"Reconcile failed: %s (message: %s)", cond.Reason, cond.Message)}
+				"Reconcile failed: %s (message: %s)", cond.Reason, errorMsg)}
 
 		case cond.Type == appv1alpha1.DeleteFailed && cond.Status == corev1.ConditionTrue:
+			errorMsg := pkgInstall.Status.UsefulErrorMessage
+			if errorMsg == "" {
+				errorMsg = cond.Message
+			}
 			return DoneApplyState{Done: true, Successful: false, Message: fmt.Sprintf(
-				"Delete failed: %s (message: %s)", cond.Reason, cond.Message)}
+				"Delete failed: %s (message: %s)", cond.Reason, errorMsg)}
 		}
 	}
 
