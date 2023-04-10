dbba784f02347ec4f76a4a97cd3931d362841b54
João Pereira <joaod@vmware.com>
2023-04-10 12:05:46 -0500

    Specify the tag being used for a release
    Signed-off-by: João Pereira <joaod@vmware.com>

--- hack/build-binaries.sh
+++ hack/build-binaries.sh
@@ -6,7 +6,7 @@ function get_latest_git_tag {
   git describe --tags | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'
 }
 
-VERSION="${1:-`get_latest_git_tag`}"
+VERSION="${1:-$(get_latest_git_tag)}"
 
 # makes builds reproducible
 export CGO_ENABLED=0
