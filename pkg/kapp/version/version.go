939c7249c2a0e2c27680718b871cc645837f26c0
João Pereira <joaod@vmware.com>
2024-03-11 09:38:54 -0500

    Make version more flexible to allow kapp to be used as a library (#897)
    Signed-off-by: João Pereira <joaod@vmware.com>

--- pkg/kapp/version/version.go
+++ pkg/kapp/version/version.go
@@ -3,4 +3,37 @@
 
 package version
 
-var Version = "develop"
+import "runtime/debug"
+
+var (
+	// Version can be set via:
+	// -ldflags="-X 'github.com/vmware-tanzu/carvel-kapp/pkg/kapp/version.Version=$TAG'"
+	defaultVersion = "develop"
+	Version        = ""
+	moduleName     = "github.com/vmware-tanzu/carvel-kapp"
+)
+
+func init() {
+	Version = version()
+}
+
+func version() string {
+	if Version != "" {
+		// Version was set via ldflags, just return it.
+		return Version
+	}
+
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return defaultVersion
+	}
+
+	// Anything else.
+	for _, dep := range info.Deps {
+		if dep.Path == moduleName {
+			return dep.Version
+		}
+	}
+
+	return defaultVersion
+}
